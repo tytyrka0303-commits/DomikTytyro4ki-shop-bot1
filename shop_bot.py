1003bdd307dcbe08041c5758b89a9f4ad8cbe48b
tytyrka0303-commits <tytyrka0303@gmail.com>
2026-02-09 17:12:08 +0300

    shop_bot.py

--- shop_bot.py
+++ shop_bot.py
@@ -1,10 +1,21 @@
 import os
 import requests
-requests.get(
-    f"https://api.telegram.org/bot{TOKEN}/sendPhoto",
-    params={
-        "chat_id": CHAT_ID,
-        "photo": "https://fortnite-api.com/images/cosmetics/br/cid_001_athena_commando_f_default/icon.png",
-        "caption": "Тест фото"
-    }
-)
+
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = -1003333614856
+
+if not TOKEN:
+    print("❌ TOKEN не найден")
+    exit()
+
+url = "https://api.telegram.org/bot" + TOKEN + "/sendPhoto"
+
+data = {
+    "chat_id": CHAT_ID,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/7/7c/Fortnite_F_letter_logo.png",
+    "caption": "✅ Бот работает, фото отправлено"
+}
+
+r = requests.post(url, data=data)
+
+print(r.text)
