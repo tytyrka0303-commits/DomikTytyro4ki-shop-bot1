46bbef9f4d7e6e981e2f047f9f9418184dae668b
tytyrka0303-commits <tytyrka0303@gmail.com>
2026-02-13 18:24:15 +0300

    shop_bot.py

--- shop_bot.py
+++ shop_bot.py
@@ -1,14 +1,13 @@
 import requests
 
 TOKEN = "8563120123:AAEhY7ahdvN-_jRYRQZ_fyVbAPzOSRPQLEE"
-CHAT_ID = "-1003333614856"
+CHAT_ID = -1003333614856
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 data = {
     "chat_id": CHAT_ID,
-    "photo": "https://upload.wikimedia.org/wikipedia/commons/7/7c/Fortnite_F_letter_logo.png",
-    "caption": "✅ Бот работает, фото отправлено"
+    "text": "✅ Бот живой и работает!"
 }
 
 r = requests.post(url, data=data)
